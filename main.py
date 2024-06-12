d860012bc4c3cac5bbd978b8a9bec8175f7aafa5
damlakayikci <damla.kayikci@boun.edu.tr>
2024-06-12 18:04:15 +0200

    refactor main

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import json
-
-
-def read_data(file):
-    file = open(file)
-    data = json.load(file)
-    return data
+import utils
 
 
 if __name__ == "__main__":
 
-    read_data("Oppositional_thinking_analysis_dataset.json")
+    data = utils.read_data("Oppositional_thinking_analysis_dataset.json")
